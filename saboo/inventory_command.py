a6b9eb1feece0d8d9880462f07e0fd6fae70f1fe
tramm <ram.tangirala@gmail.com>
2019-03-05 17:39:09 +0530

    separate methods for sales and purchase inventory

--- saboo/inventory_command.py
+++ saboo/inventory_command.py
@@ -34,10 +34,20 @@ from .tools import login,batcher,get_thread_conf
 
 _logger = logging.getLogger(__name__)
 
-class Inventory(Command):
+class SalesInventory(Command):
      
     def run(self,conf):
-        client.update_config('Modules','name','inventory') 
+        client.update_config('Modules','name','sales_inventory') 
+        _logger.warn("Running inventory should be preceded by purchase and sales order creations")
+        if  conf['xls']:
+            xls = saboo.XLS(conf)
+            _logger.debug("The xl file read in is "+ str(xls.sb['ORDERNO'].count()))
+            xls.execute()
+
+class PurchaseInventory(Command):
+     
+    def run(self,conf):
+        client.update_config('Modules','name','purchase_inventory') 
         _logger.warn("Running inventory should be preceded by purchase and sales order creations")
         if  conf['xls']:
             xls = saboo.XLS(conf)
